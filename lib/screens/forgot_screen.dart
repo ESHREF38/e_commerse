4003f5a220867c165d8c7de56fa475d32347e236
Eshref <esrefqazibeyov0538@gmail.com>
2024-05-25 22:41:12 +0400

    recovery

--- lib/screens/forgot_screen.dart
+++ lib/screens/forgot_screen.dart
@@ -1,10 +1,123 @@
+import 'package:e_commerse/screens/otp_screen.dart';
+import 'package:e_commerse/screens/recovery_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ForgotPassword extends StatelessWidget {
+class ForgotPassword extends StatefulWidget {
   const ForgotPassword({super.key});
 
   @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  bool clrButton = false;
+  TextEditingController emailController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        foregroundColor: Colors.black,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 15,
+          ),
+          child: Column(
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 10),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Forget Password",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(height: 60),
+              Text(
+                "Please enter your email address.You will receive a link to create or set a new password via email.",
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: emailController,
+                onChanged: (val) {
+                  if (val != "") {
+                    setState(() {
+                      clrButton = true;
+                    });
+                  }
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Email",
+                  suffix: InkWell(
+                    onTap: () {
+                      setState(() {
+                        emailController.clear();
+                      });
+                    },
+                    child: Icon(
+                      CupertinoIcons.multiply,
+                      color: Color(
+                        0xFFDB3022,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RecoveryScreen()),
+                  );
+                },
+                child: Text(
+                  "Send Code",
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size.fromHeight(55),
+                    backgroundColor: Color(0xFFD83022),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    )),
+              ),
+               SizedBox(height: 20,),
+               Column(
+              
+               children: [
+                 Text("OR"),
+                 TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => OTPScreen()),
+                      );
+                    },
+                    child: Text(
+                      "Verify ,Using Number?",
+                      style: TextStyle(
+                        color: Color(0xFFD83022),
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+               ],
+             ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
