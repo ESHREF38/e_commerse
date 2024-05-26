c44a1ed05fe177ddf12f371720963af9f21eb9b6
Eshref <esrefqazibeyov0538@gmail.com>
2024-05-26 23:50:20 +0400

    otpVerify

--- lib/screens/otp_screen.dart
+++ lib/screens/otp_screen.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerse/screens/otp_verifiry_screen.dart';
 import 'package:flutter/material.dart';
 
 class OTPScreen extends StatelessWidget {
@@ -5,6 +6,65 @@ class OTPScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          foregroundColor: Colors.black,
+          elevation: 0,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25),
+            child: Column(
+              children: [
+                SizedBox(height: 10),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "Forgot Password",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 60),
+                Text(
+                  "Please enter your Nu,ber.You will receive a OTP to create or set a new password via number.",
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Enter Number",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 50,),
+                ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => OTPVerifyScreen()),
+                  );
+                },
+                child: Text(
+                  "Send Code",
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size.fromHeight(55),
+                    backgroundColor: Color(0xFFD83022),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    )),
+              ),
+
+              ],
+            ),
+          ),
+        ));
   }
 }
---
+++ lib/screens/otp_verifiry_screen.dart
@@ -0,0 +1,94 @@
+import 'dart:js';
+
+import 'package:e_commerse/screens/recovery_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:sms_otp_auto_verify/sms_otp_auto_verify.dart';
+
+class OTPVerifyScreen extends StatefulWidget {
+  const OTPVerifyScreen({super.key});
+
+  @override
+  State<OTPVerifyScreen> createState() => _OTPVerifyScreenState();
+}
+
+class _OTPVerifyScreenState extends State<OTPVerifyScreen> {
+  BoxDecoration get _pinPutDecoration {
+    return BoxDecoration(
+      border: Border.all(color: Theme.of(context as BuildContext).primaryColor),
+      borderRadius: BorderRadius.circular(10.0),
+    );
+  }
+
+  TextEditingController textEditingController =
+      new TextEditingController(text: "");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Enter OTP",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 100),
+            
+              SizedBox(height: 100),
+              TextFieldPin(
+                  textController: textEditingController,
+                  autoFocus: true,
+                  codeLength: 4,
+                  alignment: MainAxisAlignment.center,
+                  defaultBoxSize: 60.0,
+                  margin: 10,
+                  selectedBoxSize: 55.0,
+                  textStyle: TextStyle(fontSize: 16),
+                  defaultDecoration: _pinPutDecoration.copyWith(
+                      border: Border.all(color: Colors.grey)),
+                  selectedDecoration: _pinPutDecoration,
+                  onChange: (code) {
+                    setState(() {});
+                  }),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RecoveryScreen()),
+                  );
+                },
+                child: Text(
+                  "Verify",
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size.fromHeight(55),
+                    backgroundColor: Color(0xFFD83022),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    )),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
